1b3394a0dde277dc38fd232ae97297ae10518788
Daniil <eo18622@yandex.ru>
2023-08-07 11:43:30 +0300

    презентер для доски

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import NewTaskButtonView from './view/new-task-button-view';
 import FilterView from './view/filter-view';
+import BoardPresenter from './presenter/board-presentor';
 import { render } from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const boardPresentor = new BoardPresenter({boardContainer: siteMainElement});
 
 render(new NewTaskButtonView(), siteHeaderElement);
 render(new FilterView(), siteMainElement);
 
+boardPresentor.init();
