d4405d933de096ed4805fe2eeb3c9dca3c9a3e31
Daniil <eo18622@yandex.ru>
2023-08-09 09:46:29 +0300

    добавляет приватный метод

--- src/presenter/board-presentor.js
+++ src/presenter/board-presentor.js
@@ -7,26 +7,36 @@ import TaskView from '../view/task-view.js';
 import LoadMoreButtonView from '../view/load-more-button-view.js';
 
 export default class BoardPresenter {
-  boardComponent = new BoardView();
-  taskListComponent = new TaskListView();
+  #boardContainer = null;
+  #tasksModel = null;
+
+  #boardComponent = new BoardView();
+  #taskListComponent = new TaskListView();
+  #boardTasks = [];
 
   constructor({boardContainer, taskModel}) {
-    this.boardContainer = boardContainer;
-    this.taskModel = taskModel;
+    this.#boardContainer = boardContainer;
+    this.#tasksModel = taskModel;
   }
 
   init() {
-    this.boardTasks = [...this.taskModel.getTasks()];
+    this.#boardTasks = [...this.#tasksModel.tasks];
 
-    render(this.boardComponent, this.boardContainer);
-    render(new SortView(), this.boardComponent.element);
-    render(this.taskListComponent, this.boardComponent.element);
-    render(new TaskEditView({task: this.boardTasks[0]}), this.taskListComponent.element);
+    render(this.#boardComponent, this.#boardContainer);
+    render(new SortView(), this.#boardComponent.element);
+    render(this.#taskListComponent, this.#boardComponent.element);
+    render(new TaskEditView({task: this.#boardTasks[0]}), this.#taskListComponent.element);
 
-    for (let i = 1; i < this.boardTasks.length; i++) {
-      render(new TaskView({task: this.boardTasks[i]}), this.taskListComponent.element);
+    for (let i = 0; i < this.#boardTasks.length; i++) {
+      this.#renderTask(new TaskView(this.#boardTasks[i]);
     }
 
-    render(new LoadMoreButtonView(), this.boardComponent.element);
+    render(new LoadMoreButtonView(), this.#boardComponent.element);
+  }
+
+  #renderTask(task) {
+    const taskComponent = new TaskView({task});
+
+    render(taskComponent, this.#taskListComponent.element);
   }
 }
