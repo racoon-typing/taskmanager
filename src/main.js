09122c830b4033abc007f3ea660ecb5a87ca42d4
Daniil <eo18622@yandex.ru>
2023-09-18 12:06:51 +0300

    create filter-model

--- src/main.js
+++ src/main.js
@@ -2,17 +2,23 @@ import { render } from './framework/render.js';
 import NewTaskButtonView from './view/new-task-button-view';
 import FilterView from './view/filter-view';
 import BoardPresenter from './presenter/board-presentor';
+import FilterPresenter from './presenter/filter-presenter.js';
 import TasksModel from './model/task-model.js';
-import { generateFilter } from './mock/filter.js';
+import FilterModel from './model/filter-model.js';
+
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
 const boardPresentor = new BoardPresenter({boardContainer: siteMainElement, tasksModel});
-
-const filters = generateFilter(tasksModel.tasks);
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteMainElement,
+  filterModel,
+  tasksModel
+});
 
 render(new NewTaskButtonView(), siteHeaderElement);
-render(new FilterView({filters}), siteMainElement);
 
+filterPresenter.init();
 boardPresentor.init();
--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,50 +1,58 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 
-function createFilterItemTemplate(filter, isChecked) {
-  const {type, count} = filter;
+function createFilterItemTemplate(filter, currentFilterType) {
+  const { type, count } = filter;
 
   return (
-    `
-      <input
-        type="radio"
-        id="filter__${type}"
-        class="filter__input visually-hidden"
-        name="filter"
-        ${isChecked ? 'checked' : ''}
-        ${count === 0 ? 'disabled' : ''}
-      />
-      <label for="filter__${type}" class="filter__label">
-        ${type}
-        <span class="filter__${type}-count">${count}</span>
-      </label>
-    `
+    `<input
+      type="radio"
+      id="filter__${type}"
+      class="filter__input visually-hidden"
+      name="filter"
+      ${type === currentFilterType ? 'checked' : ''}
+      ${count === 0 ? 'disabled' : ''}
+      value="${type}"
+    />
+    <label for="filter__${type}" class="filter__label">
+      ${type} <span class="filter__${type}-count">${count}</span></label
+    >`
   );
 }
 
-function createFilterTemplate(filterItems) {
+function createFilterTemplate(filterItems, currentFilterType) {
   const filterItemsTemplate = filterItems
-    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
+    .map((filter) => createFilterItemTemplate(filter, currentFilterType))
     .join('');
 
-  return `
-    <section class="main__filter filter container">
-      ${filterItemsTemplate}
-    </section>
-  `;
+  return (
+    `<section class="main__filter filter container">
+        ${filterItemsTemplate}
+      </section>`
+  );
 }
 
 
 export default class FilterView extends AbstractView {
   #filters = null;
+  #currentFilter = null;
+  #handleFilterTypeChange = null;
 
-  constructor({filters}) {
+  constructor({ filters, currentFilterType, onFilterTypeChange }) {
     super();
-
     this.#filters = filters;
+    this.#currentFilter = currentFilterType;
+    this.#handleFilterTypeChange = onFilterTypeChange;
+
+    this.element.addEventListener('change', this.#filterTypeChangeHandler);
   }
 
   get template() {
-    return createFilterTemplate(this.#filters);
+    return createFilterTemplate(this.#filters, this.#currentFilter);
   }
+
+  #filterTypeChangeHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleFilterTypeChange(evt.target.value);
+  };
 }
